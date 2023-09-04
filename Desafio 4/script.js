b271637b178bc350177693b6a0f0ae89f410fb79
gabrielandersonolv <gabrielandersonolv2@gmail.com>
2023-09-04 15:13:43 -0300

    04/09/2023

---
+++ Desafio 4/script.js
@@ -0,0 +1,9 @@
+// var formulario = document.querySelector('#input');
+
+
+
+function limparInputs(){
+    document.getElementById('input1').value=''
+    document.getElementById('input2').value=''
+    alert("Bom te ver de volta!!")
+  }
